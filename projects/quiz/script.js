1cc8fd505a013e424ce53ed6cf8a77c08c4e85a5
David <calonnedavid@gmail.com>
2024-09-14 12:19:44 +0200

    Html, Css, Js update

--- projects/quiz/script.js
+++ projects/quiz/script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Your Javascript here
-});                                                   
+    const root = document.getElementById('root');
+});
